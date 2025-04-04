cab4fa7354051fbac820fb17eec6b8c6cc310b4b
Robinisha Guyton <Robinisha Guyton@LAPTOP-HDR8OM3E>
2025-04-03 18:18:06 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class EventRegistration
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Range(0.01, 10000)]
+
+        public decimal Price { get; set; }
+
+        [Required]
+
+        public string Category { get; set; }
+
+    }
+}
